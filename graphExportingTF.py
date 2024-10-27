1c542d3008844ca353eb4cfb209c25a4aa48de58
batmandor <29897209+BatMandor@users.noreply.github.com>
2024-10-27 19:26:43 +0000

    Fixed structure

--- graphExportingTF.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-import tensorflow as tf
-from matplotlib import pyplot as plt
-import numpy as np
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-if physical_devices:
-  tf.config.experimental.set_memory_growth(physical_devices[0], True)
-loaded = tf.saved_model.load("/tmp/mobilenet/1/")
-print(list(loaded.signatures.keys()))
